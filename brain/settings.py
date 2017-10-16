b3cc20444bd749b8b23c99eefb1cb0f956a732ff
Abdul-Rehman-Yousaf <abdul12391@gmail.com>
2017-10-16 16:49:28 +0500

    Add app.yaml4

--- brain/settings.py
+++ brain/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # For a test push to trigger build from codeship.
-# Second thing for codeship deployment push
+# Second thing for CodeShip deployment push
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
